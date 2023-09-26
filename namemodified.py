cee9cc6d61261b822c4ca541992eb22a62a66fe6
Tatjana <g00425678@atu.ie>
2023-09-26 20:31:15 +0200

    added comment to namemodified

--- namemodified.py
+++ namemodified.py
@@ -1,3 +1,6 @@
+#https://stackoverflow.com/questions/62544969/creating-a-greeting-program-which-only-accepts-specific-users-and-invalidates-an
+
+
 names = ("Alice", "Bob")
 name = input("Please type your name:")
 if name == "Alice":
