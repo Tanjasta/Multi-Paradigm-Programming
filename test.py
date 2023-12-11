86c4d2257faf5bce2b72771078cbd8a05ce0369f
Tatjana <g00425678@atu.ie>
2023-12-11 16:00:47 +0100

    some code manupulation

--- test.py
+++ test.py
@@ -33,17 +33,31 @@ def read_customer(file_path):
 
 
 def process_order(shop, customer):
+    total_order_cost = 0
+    
     for product in customer["products"]:
         for shop_product in shop["products"]:
             if product["name"] == shop_product["name"]:
-                if product["quantity"] <= shop_product["quantity"]:
-                    shop["cash"] += product["quantity"] * shop_product["price"]
+                order_cost = product["quantity"] * shop_product["price"]
+                
+                if product["quantity"] <= shop_product["quantity"] and customer["cash"] >= order_cost:
+                    shop["cash"] += order_cost
                     shop_product["quantity"] -= product["quantity"]
+                    total_order_cost += order_cost
                     print(f"Order processed for {product['name']} quantity: {product['quantity']}")
-                else:
+                elif product["quantity"] > shop_product["quantity"]:
                     print(f"Not enough stock for {product['name']}")
+                else:
+                    print(f"Not enough money for {product['name']}")
                 break
-    print(f"Remaining cash in the shop: {shop['cash']}")
+    
+    if total_order_cost > 0:
+        print(f"Total order cost: {total_order_cost}")
+        print(f"Remaining cash in the shop: {shop['cash']}")
+    else:
+        print("No valid orders processed.")
+
+
 
 
 def print_product(product):
